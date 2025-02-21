becaed931ce188b1be3a0dc0175aadaae6f95095
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-02-21 12:47:18 +0530

    test: fixed upgrade test failure

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -72,6 +72,47 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 	return options
 }
 
+// Provision KMS - Key Protect to use in DA tests
+func setupKMSKeyProtect(t *testing.T, region string) *terraform.Options {
+	prefix := "wxd-da-key"
+	realTerraformDir := "./kp-instance"
+	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
+
+	checkVariable := "TF_VAR_ibmcloud_api_key"
+	val, present := os.LookupEnv(checkVariable)
+	require.True(t, present, checkVariable+" environment variable not set")
+	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
+
+	logger.Log(t, "Tempdir: ", tempTerraformDir)
+	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: tempTerraformDir,
+		Vars: map[string]interface{}{
+			"prefix": prefix,
+			"region": region,
+		},
+		Upgrade: true,
+	})
+
+	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
+	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
+	require.NoError(t, existErr, "Init and Apply of temp resources (KP Instance and Key creation) failed")
+
+	return existingTerraformOptions
+}
+
+func cleanupResources(t *testing.T, terraformOptions *terraform.Options) {
+	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
+	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
+	// Destroy the temporary existing resources if required
+	if t.Failed() && strings.ToLower(envVal) == "true" {
+		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
+	} else {
+		logger.Log(t, "START: Destroy (existing resources)")
+		terraform.Destroy(t, terraformOptions)
+		logger.Log(t, "END: Destroy (existing resources)")
+	}
+}
+
 func TestRunBasicExample(t *testing.T) {
 	t.Parallel()
 
@@ -138,111 +179,56 @@ func TestRunExistingResourcesExample(t *testing.T) {
 			assert.NotNil(t, output, "Expected some output")
 		}
 	}
-
-	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
-	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
-	// Destroy the temporary existing resources if required
-	if t.Failed() && strings.ToLower(envVal) == "true" {
-		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
-	} else {
-		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, existingTerraformOptions)
-		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
-		logger.Log(t, "END: Destroy (existing resources)")
-	}
+	cleanupResources(t, existingTerraformOptions)
 }
 
-// Test the DA
 func TestRunStandardSolution(t *testing.T) {
 	t.Parallel()
-
-	// ---------------------------------------------------------
-	// Provision KMS - Key Protect
-	// ---------------------------------------------------------
-
 	var region = validRegions[rand.Intn(len(validRegions))]
-
-	prefix := "wx-da"
-	realTerraformDir := "./kp-instance"
-	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
-
-	// Verify ibmcloud_api_key variable is set
-	checkVariable := "TF_VAR_ibmcloud_api_key"
-	val, present := os.LookupEnv(checkVariable)
-	require.True(t, present, checkVariable+" environment variable not set")
-	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
-
-	logger.Log(t, "Tempdir: ", tempTerraformDir)
-	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: tempTerraformDir,
-		Vars: map[string]interface{}{
-			"prefix": prefix,
-			"region": region,
-		},
-		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
-		// This is the same as setting the -upgrade=true flag with terraform.
-		Upgrade: true,
-	})
-
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
-
-	if existErr != nil {
-		assert.True(t, existErr == nil, "Init and Apply of temp resources (KP Instance and Key creation) failed")
-	} else {
-		// ------------------------------------------------------------------------------------
-		// Deploy watsonx.data DA using existing KP details
-		// ------------------------------------------------------------------------------------
-
-		options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-			Testing:       t,
-			TerraformDir:  standardSolutionTerraformDir,
-			Prefix:        "wxd-da",
-			Region:        region,
-			ResourceGroup: resourceGroup,
-		})
-		options.TerraformVars = map[string]interface{}{
-			"prefix":                      options.Prefix,
-			"region":                      options.Region,
-			"use_existing_resource_group": true,
-			"resource_group_name":         terraform.Output(t, existingTerraformOptions, "resource_group_name"),
-			"provider_visibility":         "public",
-			"existing_kms_instance_crn":   terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
-		}
-
-		output, err := options.RunTestConsistency()
-		assert.Nil(t, err, "This should not have errored")
-		assert.NotNil(t, output, "Expected some output")
-	}
-
-	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
-	// Destroy the temporary resources created
-	if t.Failed() && strings.ToLower(envVal) == "true" {
-		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
-	} else {
-		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, existingTerraformOptions)
-		logger.Log(t, "END: Destroy (existing resources)")
-	}
-}
-
-func TestRunStandardUpgradeSolution(t *testing.T) {
-	t.Parallel()
+	existingTerraformOptions := setupKMSKeyProtect(t, region)
 
 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
 		Testing:       t,
 		TerraformDir:  standardSolutionTerraformDir,
-		Region:        validRegions[rand.Intn(len(validRegions))],
-		Prefix:        "wxa-da-upg",
+		Prefix:        "wxd-da",
+		Region:        region,
 		ResourceGroup: resourceGroup,
 	})
 	options.TerraformVars = map[string]interface{}{
 		"prefix":                      options.Prefix,
 		"region":                      options.Region,
 		"use_existing_resource_group": true,
-		"resource_group_name":         options.Prefix,
+		"resource_group_name":         terraform.Output(t, existingTerraformOptions, "resource_group_name"),
 		"provider_visibility":         "public",
-		"existing_kms_instance_crn":   permanentResources["hpcs_south_crn"],
+		"existing_kms_instance_crn":   terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
+	}
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+
+	cleanupResources(t, existingTerraformOptions)
+}
+
+func TestRunStandardUpgradeSolution(t *testing.T) {
+	t.Parallel()
+	var region = validRegions[rand.Intn(len(validRegions))]
+	existingTerraformOptions := setupKMSKeyProtect(t, region)
+
+	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+		Testing:       t,
+		TerraformDir:  standardSolutionTerraformDir,
+		Prefix:        "wxd-da-upg",
+		Region:        region,
+		ResourceGroup: resourceGroup,
+	})
+	options.TerraformVars = map[string]interface{}{
+		"prefix":                      options.Prefix,
+		"region":                      options.Region,
+		"use_existing_resource_group": true,
+		"resource_group_name":         terraform.Output(t, existingTerraformOptions, "resource_group_name"),
+		"provider_visibility":         "public",
+		"existing_kms_instance_crn":   terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
 	}
 
 	output, err := options.RunTestUpgrade()
@@ -250,4 +236,6 @@ func TestRunStandardUpgradeSolution(t *testing.T) {
 		assert.Nil(t, err, "This should not have errored")
 		assert.NotNil(t, output, "Expected some output")
 	}
+
+	cleanupResources(t, existingTerraformOptions)
 }
