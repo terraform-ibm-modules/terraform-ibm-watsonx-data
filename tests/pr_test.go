53c30a78ecd0d9e9c957c7afbf98cd70947d6294
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2025-03-05 16:32:03 +0530

    Update DA to only support using existing resource group

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -199,12 +199,12 @@ func TestRunStandardSolution(t *testing.T) {
 		ResourceGroup: resourceGroup,
 	})
 	options.TerraformVars = map[string]interface{}{
-		"prefix":                      options.Prefix,
-		"region":                      options.Region,
-		"use_existing_resource_group": true,
-		"resource_group_name":         terraform.Output(t, existingTerraformOptions, "resource_group_name"),
-		"provider_visibility":         "public",
-		"existing_kms_instance_crn":   terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
+		"prefix":                       options.Prefix,
+		"region":                       options.Region,
+		"existing_resource_group_name": resourceGroup,
+		"resource_group_name":          terraform.Output(t, existingTerraformOptions, "resource_group_name"),
+		"provider_visibility":          "public",
+		"existing_kms_instance_crn":    terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
 	}
 
 	output, err := options.RunTestConsistency()
@@ -229,12 +229,12 @@ func TestRunStandardUpgradeSolution(t *testing.T) {
 		ResourceGroup: resourceGroup,
 	})
 	options.TerraformVars = map[string]interface{}{
-		"prefix":                      options.Prefix,
-		"region":                      options.Region,
-		"use_existing_resource_group": true,
-		"resource_group_name":         terraform.Output(t, existingTerraformOptions, "resource_group_name"),
-		"provider_visibility":         "public",
-		"existing_kms_instance_crn":   terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
+		"prefix":                       options.Prefix,
+		"region":                       options.Region,
+		"existing_resource_group_name": resourceGroup,
+		"resource_group_name":          terraform.Output(t, existingTerraformOptions, "resource_group_name"),
+		"provider_visibility":          "public",
+		"existing_kms_instance_crn":    terraform.Output(t, existingTerraformOptions, "key_protect_crn"),
 	}
 
 	output, err := options.RunTestUpgrade()
