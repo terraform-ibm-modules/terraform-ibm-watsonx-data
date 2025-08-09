5ba529fac133aabc5b784370765c583d17ee4922
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-09 17:09:31 +0000

    fix(deps): update terraform terraform-ibm-modules/kms-all-inclusive/ibm to v5.1.19

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -22,7 +22,7 @@ locals {
 module "key_protect_all_inclusive" {
   count                     = var.enable_kms_encryption ? 1 : 0
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.1.18"
+  version                   = "5.1.19"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
