eaebd7a06f8ba18b08d791e114c1a4fa8a7bbeac
Arya Girish K <arya.girish.k@ibm.com>
2025-10-17 00:41:01 +0530

    feat: Add UI regex validation

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -28,8 +28,8 @@ variable "existing_resource_group_name" {
 variable "prefix" {
   type        = string
   nullable    = true
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: wx-0205-data. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)"
-
+  description = "The prefix to add to all resources that this solution creates (e.g `prod`, `test`, `dev`). To skip using a prefix, set this value to null or an empty string. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
+  
   validation {
     condition = var.prefix == null || var.prefix == "" ? true : alltrue([
       can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)), length(regexall("--", var.prefix)) == 0
