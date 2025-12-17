8cda57f535bda023393842c264b3f71285e58345
Zikra Iqbal <35029744+zikra-iqbal@users.noreply.github.com>
2025-12-17 16:37:09 +0530

    feat: Added "Next steps" to the DA (#183)

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -31,3 +31,28 @@ output "dashboard_url" {
   description = "The dashboard URL of the watsonx.data instance."
   value       = module.watsonx_data.dashboard_url
 }
+
+output "next_steps_text" {
+  value       = "You can now use watsonx.data to integrate and analyze all your data through a single, unified data platform."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to the watsonx.data dashboard"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = module.watsonx_data.dashboard_url
+  description = "Primary URL"
+}
+
+output "next_step_secondary_label" {
+  value       = "Learn more about watsonx.data"
+  description = "Secondary label"
+}
+
+output "next_step_secondary_url" {
+  value       = "https://cloud.ibm.com/docs/watsonxdata?topic=watsonxdata-wxd_ov"
+  description = "Secondary URL"
+}
