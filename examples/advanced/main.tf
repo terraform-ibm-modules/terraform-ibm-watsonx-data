586ab112104df40115543095cf20e87b61ed0c95
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-26 23:40:01 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.0"
+  version = "1.4.7"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? local.resource_group_name : null
   existing_resource_group_name = var.resource_group
@@ -29,7 +29,7 @@ module "resource_group" {
 module "key_protect_all_inclusive" {
   count                     = var.enable_kms_encryption ? 1 : 0
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.5.0"
+  version                   = "5.5.11"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = local.key_protect_instance_name
