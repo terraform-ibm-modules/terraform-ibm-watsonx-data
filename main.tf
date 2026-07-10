e6e9ab4c097fd73c2427c594cc38f5ad912a8610
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-07-10 22:58:23 +0530

    chore(deps): update module github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper to v1.76.3 (#259)
    * chore(deps): update module github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper to v1.76.3
    
    * fix: sort access tags to prevent hash mismatch in consistency checks
    
    ---------
    
    Co-authored-by: Harika Ponna <harika.ponna@ibm.com>

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ data "ibm_iam_access_tag" "access_tag" { # tflint-ignore: terraform_unused_decla
 resource "ibm_resource_tag" "watsonx_data_tag" {
   count       = length(var.access_tags) != 0 ? 1 : 0
   resource_id = local.watsonx_data_crn
-  tags        = var.access_tags
+  tags        = sort(var.access_tags)
   tag_type    = "access"
 }
 
