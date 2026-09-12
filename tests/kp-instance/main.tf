bd8a5902218c4e5d3bffcbd9eaf2c0a41a693c63
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-12 08:22:59 +0000

    fix(deps): update dependencies

--- tests/kp-instance/main.tf
+++ tests/kp-instance/main.tf
@@ -7,7 +7,7 @@ module "resource_group" {
 
 module "kms" {
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.6.7"
+  version                     = "5.6.8"
   create_key_protect_instance = true
   key_protect_instance_name   = "${var.prefix}-kp"
   resource_group_id           = module.resource_group.resource_group_id
